20c9e08f00a313d877f18383f3cb6976f616f6c2
maicol manuel orosco vasquez <morosco@tinnto.com>
2024-05-15 16:02:44 -0500

    successs

--- src/components/advertisers/ContactFormNeg.tsx
+++ src/components/advertisers/ContactFormNeg.tsx
@@ -11,12 +11,11 @@ interface IForm {
 
 interface Props {
     title?: string;
+    consulta?: boolean;
 }
 
-const ContactFormNeg = ({ title='Contáctanos' }: Props) => {
-    // const [recaptchaResponse, setRecaptchaResponse] = useState<null | {
-    //     score: number;
-    // }>(null);
+const ContactFormNeg = ({ title = 'Contáctanos', consulta = true }: Props) => {
+    const [success, setSuccess] = useState(false);
     const [loading, setLoading] = useState(false);
     const [form, setForm] = useState<IForm>({
         nombre: "",
@@ -66,7 +65,7 @@ const ContactFormNeg = ({ title='Contáctanos' }: Props) => {
         if (form.correo && !isValidateEmail(form.correo)) {
             errors.correo = "Ingresa un correo válido";
         }
-        if (form.consulta.length < 1) {
+        if (form.consulta.length < 1 && consulta) {
             errors.consulta = "Ingresa una consulta válida";
         }
 
@@ -74,15 +73,49 @@ const ContactFormNeg = ({ title='Contáctanos' }: Props) => {
         return Object.values(errors).some((error) => error.length > 0);
     }
 
-    const onSubmitNeg = (e: any) => {
+    const onSubmitNeg = async (e: any) => {
         e.preventDefault();
+        let body = {
+            tipo: consulta ? "ADs" : "PLAN",
+            nombres: form.nombre,
+            apellidos: form.apellido,
+            numero: form.celular,
+            email: form.correo,
+            consulta: consulta ? form.consulta : "Planes de pago"
+        };
+        setSuccess(false);
         setLoading(true);
-        console.log("hola")
-        setTimeout(() => {
+        // setTimeout(() => {
+        //     setLoading(false);
+        // }, 2000);
+        if (validateForm()) {
             setLoading(false);
-        }, 2000);
-        if (validateForm()) return;
+            return;
+        }
+        // https://dev.api.mennu.net/v1/core/api/contact/save | POST
+
+        await fetch('https://dev.api.mennu.net/v1/core/api/contact/save', {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(body),
+        }).then(response => response.json())
+            .then(data => {
+                console.log('Success:', data);
+                setSuccess(true);
+                setLoading(false);
+                setTimeout(() => {
+                    setSuccess(false);
+                }, 3000);
+            })
+            .catch((error) => {
+                console.error('Error:', error);
+                setLoading(false);
+                setSuccess(false);
+            });
     }
+
 
     return (
         <>
@@ -98,8 +131,7 @@ const ContactFormNeg = ({ title='Contáctanos' }: Props) => {
                                 id="nombre"
                                 value={form.nombre}
                                 name="nombre"
-                                class={`bg-[#EEEEEE] h-9 p-2 appearance-none leading-tight focus:outline-none focus:shadow-none font-hind font-normal text-sm
-                            ${errors.nombre ? "bg-[#FFEFED] border-[#F1998E]" : ""}
+                                class={`bg-[#EEEEEE] h-9 p-2 appearance-none leading-tight focus:outline-none focus:shadow-none font-hind font-normal text-sm ${errors.nombre ? "bg-[#FFEFED] border-[#F1998E]" : ""}
                             `}
                                 placeholder="Ingresa tu nombre"
                                 onChange={handleChange}
@@ -117,8 +149,7 @@ const ContactFormNeg = ({ title='Contáctanos' }: Props) => {
                                 id="apellido"
                                 value={form.apellido}
                                 name="apellido"
-                                class={`bg-[#EEEEEE] h-9 p-2 appearance-none leading-tight focus:outline-none focus:shadow-none font-hind font-normal text-sm
-                            ${errors.nombre ? "bg-[#FFEFED] border-[#F1998E]" : ""}
+                                class={`bg-[#EEEEEE] h-9 p-2 appearance-none leading-tight focus:outline-none focus:shadow-none font-hind font-normal text-sm ${errors.nombre ? "bg-[#FFEFED] border-[#F1998E]" : ""}
                             `}
                                 placeholder="Ingresa tus apellidos"
                                 onChange={handleChange}
@@ -161,8 +192,7 @@ const ContactFormNeg = ({ title='Contáctanos' }: Props) => {
                             name="correo"
                             value={form.correo}
                             // class="bg-[#EEEEEE] h-9 p-2 appearance-none leading-tight focus:outline-none focus:shadow-none font-hind font-normal text-sm"
-                            class={`bg-[#EEEEEE] h-9 p-2 appearance-none leading-tight focus:outline-none focus:shadow-none font-hind font-normal text-sm
-                        ${errors.correo ? "bg-[#FFEFED] border-[#F1998E]" : ""}
+                            class={`bg-[#EEEEEE] h-9 p-2 appearance-none leading-tight focus:outline-none focus:shadow-none font-hind font-normal text-sm ${errors.correo ? "bg-[#FFEFED] border-[#F1998E]" : ""}
                         `}
                             placeholder="Correo electrónico (opcional)"
                             onChange={handleChange}
@@ -173,27 +203,29 @@ const ContactFormNeg = ({ title='Contáctanos' }: Props) => {
                             </Typography>
                         }
                     </label>
-                    <label for="consulta" class="flex flex-col gap-1">
-                        <Typography variant="label" weight="small">
-                            Indícanos cuál es tu consulta
-                        </Typography>
-                        <textarea
-                            id="consulta"
-                            name="consulta"
-                            value={form.consulta}
-                            // class="bg-[#EEEEEE] h-24 p-2 appearance-none leading-tight focus:outline-none focus:shadow-none font-hind font-normal text-sm"
-                            class={`bg-[#EEEEEE] h-24 p-2 appearance-none leading-tight focus:outline-none focus:shadow-none font-hind font-normal text-sm
-                        ${errors.consulta ? "bg-[#FFEFED] border-[#F1998E]" : ""}
-                        `}
-                            placeholder="Escribe el detalle de tu consulta"
-                            onChange={handleChange}
-                        ></textarea>
-                        {
-                            errors.consulta && <Typography variant="paragraph" weight="small" color="complementaryII">
-                                {errors.consulta}
-                            </Typography>
-                        }
-                    </label>
+                    {
+                        consulta && <>
+                            <label for="consulta" class="flex flex-col gap-1">
+                                <Typography variant="label" weight="small">
+                                    Indícanos cuál es tu consulta
+                                </Typography>
+                                <textarea
+                                    id="consulta"
+                                    name="consulta"
+                                    value={form.consulta}
+                                    // class="bg-[#EEEEEE] h-24 p-2 appearance-none leading-tight focus:outline-none focus:shadow-none font-hind font-normal text-sm"
+                                    class={`bg-[#EEEEEE] h-24 p-2 appearance-none leading-tight focus:outline-none focus:shadow-none font-hind font-normal text-sm ${errors.consulta ? "bg-[#FFEFED] border-[#F1998E]" : ""}`}
+                                    placeholder="Escribe el detalle de tu consulta"
+                                    onChange={handleChange}
+                                ></textarea>
+                                {
+                                    errors.consulta && <Typography variant="paragraph" weight="small" color="complementaryII">
+                                        {errors.consulta}
+                                    </Typography>
+                                }
+                            </label>
+                        </>
+                    }
                     <button
                         class="bg-primary text-white h-14 w-full lg:w-80 disabled:bg-[#BDBDBD] disabled:cursor-not-allowed disabled:opacity-50"
                         type="submit"
@@ -210,6 +242,21 @@ const ContactFormNeg = ({ title='Contáctanos' }: Props) => {
                         </Typography>
                     </button>
                 </form>
+                <div class={`flex flex-row gap-2 items-center justify-center ${success ? "opacity-100" : "opacity-0"}`}>
+                    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <mask id="mask0_27731_5361" style="mask-type:alpha" maskUnits="userSpaceOnUse" x="0" y="0" width="24" height="24">
+                            <rect width="24" height="24" fill="#D9D9D9" />
+                        </mask>
+                        <g mask="url(#mask0_27731_5361)">
+                            <path d="M10.5808 16.2538L17.3038 9.53075L16.25 8.47693L10.5808 14.1462L7.73075 11.2962L6.67693 12.35L10.5808 16.2538ZM12.0016 21.5C10.6877 21.5 9.45268 21.2506 8.29655 20.752C7.1404 20.2533 6.13472 19.5765 5.2795 18.7217C4.42427 17.8669 3.74721 16.8616 3.24833 15.706C2.74944 14.5504 2.5 13.3156 2.5 12.0017C2.5 10.6877 2.74933 9.45268 3.248 8.29655C3.74667 7.1404 4.42342 6.13472 5.27825 5.2795C6.1331 4.42427 7.13834 3.74721 8.29398 3.24833C9.44959 2.74944 10.6844 2.5 11.9983 2.5C13.3122 2.5 14.5473 2.74933 15.7034 3.248C16.8596 3.74667 17.8652 4.42342 18.7205 5.27825C19.5757 6.1331 20.2527 7.13834 20.7516 8.29398C21.2505 9.44959 21.5 10.6844 21.5 11.9983C21.5 13.3122 21.2506 14.5473 20.752 15.7034C20.2533 16.8596 19.5765 17.8652 18.7217 18.7205C17.8669 19.5757 16.8616 20.2527 15.706 20.7516C14.5504 21.2505 13.3156 21.5 12.0016 21.5Z" fill="#3AA76D" />
+                        </g>
+                    </svg>
+
+                    <Typography variant="label" weight="large">
+                        ¡Enviado exitosamente!
+                    </Typography>
+                </div>
+
             </div>
         </>
     )
