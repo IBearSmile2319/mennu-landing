720e6beb330c34b320b6f1f2d8dc557b1b04d1ac
Maicol Manuel Orosco Vasquez <78711486+IBearSmile2319@users.noreply.github.com>
2024-07-18 14:06:40 -0500

    falto la s

--- src/components/advertisers/ContactFormNeg.tsx
+++ src/components/advertisers/ContactFormNeg.tsx
@@ -147,7 +147,7 @@ const ContactFormNeg = ({ title = 'Contáctanos', consulta = true }: Props) => {
                             }
                         </label>
                         <label for="apellido" class="flex flex-col gap-1 w-full">
-                            <Typography variant="label" weight="small"> Apellido </Typography>
+                            <Typography variant="label" weight="small"> Apellidos </Typography>
                             <input
                                 type="text"
                                 id="apellido"
